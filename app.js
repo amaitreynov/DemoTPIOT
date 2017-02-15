f4a1b50040adcb4d9d1157d15f897ac41b020ae5
amaitre <antoine.maitre@ynov.com>
2017-02-15 17:05:34 +0100

    [admin] added create impl + bootstrap

--- app.js
+++ app.js
@@ -1,58 +1,37 @@
 'use strict';
 
-var express = require('express');
-var app = express();
-var swig = require('swig');
+let express = require('express');
+let app = express();
+let swig = require('swig');
 
-var Protocol = require('azure-iot-device-amqp').Amqp;
-var Client = require('azure-iot-device').Client;
-var ConnectionString = require('azure-iot-device').ConnectionString;
-var Message = require('azure-iot-device').Message;
-var IotHub = require('azure-iothub');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var parser = require('body-parser');
-var http1 = require('request');
+let Protocol = require('azure-iot-device-amqp').Amqp;
+let Client = require('azure-iot-device').Client;
+let ConnectionString = require('azure-iot-device').ConnectionString;
+let Message = require('azure-iot-device').Message;
+let iotHub = require('azure-iothub');
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let bodyParser = require('body-parser');
+let http1 = require('request');
 
-var connectionString = 'HostName=ynovIotHubTonio.azure-devices.net;DeviceId=new-device_01;SharedAccessKey=iaINBHMPhghQzyCEn5+RauTbJniCDl5fp7qGsB+9NeU=';
-var connectionStringIotHub = 'HostName=ynovIotHubTonio.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=tn+b+mToX2wTSZNXenQzG5seBnPadl7ABmJMVrcOd64=';
+let connectionString = 'HostName=ynovIotHubTonio.azure-devices.net;DeviceId=new-device_01;SharedAccessKey=iaINBHMPhghQzyCEn5+RauTbJniCDl5fp7qGsB+9NeU=';
+let connectionStringIotHub = 'HostName=ynovIotHubTonio.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=tn+b+mToX2wTSZNXenQzG5seBnPadl7ABmJMVrcOd64=';
 
-var registry = IotHub.Registry.fromConnectionString(connectionStringIotHub);
-var deviceId = ConnectionString.parse(connectionString).DeviceId;
+let registry = iotHub.Registry.fromConnectionString(connectionStringIotHub);
+let deviceId = ConnectionString.parse(connectionString).DeviceId;
 
+// This is where all the magic happens!
+app.engine('html', swig.renderFile);
 
-
-
-// String containing Hostname, Device Id & Device Key in the following formats:
-//  "HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
-// var connectionString = 'HostName=agirtest4389e.azure-devices.net;DeviceId=Ubuntu01;SharedAccessKey=jJkGh1Jzd3eSMj/JqeURKw==';
-// var connectionString = 'HostName=HubDemo.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=PYTJXWo07Ih9140NA++Sh6YjDb+POJLgnXWoL/nl0U0=';
-/**
- * Antoine's hub
- * @type {string}
- * HostName=ynovIpiIot.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=NfD1qL4F+51zr8HUcTtqGku7gW8Dl8o8XF0myBw4fIk=
- */
-
-/**
- * Sylvain's device
- * @type {string}
- */
-// var connectionString = 'HostName=HubDemo.azure-devices.net;DeviceId=custom_new_device_01;SharedAccessKey=dXtd8bUjJzJnXMoxRb3mcFKpu+pS4neoCoSiUtVHFig=';
-
-/**
- * Antoine's device
- * HostName=ynovIpiIot.azure-devices.net;DeviceId=new_device_01;SharedAccessKey=DXSkfz78lGjADuuCJ6L2/h8p8rqeewz9/Hx/gKGi9bc=
- */
-
-
-// Create IoT Hub client
-// var client = Client.fromConnectionString(connectionString, Protocol);
-
-
-
+app.set('view engine', 'html');
+app.set('views', __dirname + '/Public');
+app.set('view cache', false);
+swig.setDefaults({cache: false});
 
 //app.use(express.static(__dirname + '/public'));
 //app.use(express.static(__dirname + '/socket.io'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function (req, res) {
     //res.send('Hello World!');
@@ -74,12 +53,50 @@ app.get('/admin', function (req, res, next) {
     })
 });
 
+app.get('/admin/create', function (req, res, next) {
+    res.render('createDevice');
+});
+
+app.post('/admin/create', function (req, res, next) {
+    let error;
+    const deviceId = req.body.deviceId;
+    let device = new iotHub.Device(null);
+    device.deviceId = deviceId;
+    registry.create(device, function(err, deviceInfo, res) {
+        if (err) {
+            registry.get(device.deviceId, printDeviceInfo(err, deviceInfo, res));
+        }
+        if (deviceInfo) {
+            const deviceId = deviceInfo.deviceId;
+            const deviceKey = deviceInfo.authentication.symmetricKey.primaryKey;
+
+            printDeviceInfo(err, deviceInfo, res)
+        }
+    });
+
+    function printDeviceInfo(err, deviceInfo, res) {
+        if(err) {
+            error = err;
+            res.render('error', {error: err});
+        }
+        if (deviceInfo) {
+            console.log('Device ID: ' + deviceInfo.deviceId);
+            console.log('Device key: ' + deviceInfo.authentication.symmetricKey.primaryKey);
+        }
+    }
+    if(!error) {
+        //if no error occured, redirecting to admin panel after 1s timeout so we have the time to get the created device
+        setTimeout(function () {
+            res.redirect('/admin');
+        }, 1000);
+    }
+});
 
 
 io.on('connection', function (socket) {
     socket.on('message', function (data) {
         console.log(data);
-        var args = {
+        const args = {
             url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/languages?numberOfLanguagesToDetect=1",
             headers: {
                 "Content-Type": "application/json",
@@ -99,35 +116,35 @@ io.on('connection', function (socket) {
             if (error) {
                 return console.error('upload failed:', error);
             }
-            var data1 = body.documents[0];
+            let data1 = body.documents[0];
             console.log('Upload successful!  Server responded with:', data1);
 
             socket.send(JSON.stringify({result: "OK, bien reçu, le message était dans la langue : " + data1.detectedLanguages[0].name}));
 
             // use factory function from AMQP-specific package
-            var clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
+            const clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
 
             // AMQP-specific factory function returns Client object from core package
-            var client = clientFromConnectionString(connectionString);
+            const client = clientFromConnectionString(connectionString);
 
             // use Message object from core package
-            var Message = require('azure-iot-device').Message;
+            const Message = require('azure-iot-device').Message;
 
-            var connectCallback = function (err) {
+            const connectCallback = function (err) {
                 if (err) {
                     console.error('Could not connect: ' + err);
                 } else {
 
                     console.log('Client connected');
                     console.log('Data1: ' + JSON.stringify(data1));
-                    var dataToSend = {};
+                    let dataToSend = {};
                     try {
                         dataToSend = JSON.stringify({deviceId: "new-device_01", data: data1.detectedLanguages[0].name});
                     } catch (e) {
                         console.error(e);
                         throw e;
                     }
-                    var msg = new Message(dataToSend);
+                    let msg = new Message(dataToSend);
                     console.log('trying to send msg: ' + JSON.stringify(msg));
                     client.sendEvent(msg, function (err) {
                         if (err) {
@@ -143,15 +160,7 @@ io.on('connection', function (socket) {
     });
 });
 
-// This is where all the magic happens!
-app.engine('html', swig.renderFile);
-
-app.set('view engine', 'html');
-app.set('views', __dirname + '/Public');
-app.set('view cache', false);
-swig.setDefaults({cache: false});
-
-var port = process.env.port || 1337;
+const port = process.env.port || 1337;
 http.listen(port, function (err) {
     if (err)
         console.log("Err while starting server:" + err);
